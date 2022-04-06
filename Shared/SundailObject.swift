dbd72cc6f8535612ee73f637852813bf26c575c6
Leo Dion <leogdion@brightdigit.com>
2022-04-06 19:17:57 -0400

    refactoring files

--- Shared/SundailObject.swift
+++ Shared/SundailObject.swift
@@ -1,134 +1,98 @@
-//
-//  SundailObject.swift
-//  WatchConnectivityTest
-//
-//  Created by Leo Dion on 4/5/22.
-//
-
+import Combine
 import Foundation
 import SwiftUI
 import WatchConnectivity
-import Combine
 
-
-struct MissingCompanionError : Error {
-  
-}
-extension Publisher {
-  func assignOnMain(to published: inout Published<Self.Output>.Publisher) where Self.Failure == Never {
-    self.receive(on: DispatchQueue.main).assign(to: &published)
-  }
-  
-  func assignOnMain<OutputType>(_ keyPath: KeyPath<Self.Output, OutputType>, to published: inout Published<OutputType>.Publisher) where Self.Failure == Never {
-    self.map(keyPath).assign(to: &published)
-  }
-  
-  func neverPublishers() -> (AnyPublisher<Output, Never>, AnyPublisher<Failure, Never>) {
-    let failurePublisher = self.share().map{ _ in nil as Failure? }.catch { error in
-      return Just(error)
-    }.compactMap{$0}.eraseToAnyPublisher()
-    
-    let successPublisher = self.share().map{$0 as Output?}.replaceError(with: nil).compactMap{$0}.eraseToAnyPublisher()
-    
-    return (successPublisher, failurePublisher)
-  }
-  
-  
-}
-
-class SundailObject : ObservableObject {
-   
+class SundailObject: ObservableObject {
   #if os(iOS)
-  static let isCompanionAppInstalledKeyPath : KeyPath<WCSession, Bool> = \.isWatchAppInstalled
+    static let isCompanionAppInstalledKeyPath: KeyPath<WCSession, Bool> = \.isWatchAppInstalled
   #elseif os(watchOS)
-  static let isCompanionAppInstalledKeyPath : KeyPath<WCSession, Bool> = \.isCompanionAppInstalled
+    static let isCompanionAppInstalledKeyPath: KeyPath<WCSession, Bool> = \.isCompanionAppInstalled
   #endif
-  
+
   let lastColorSendingSubject = PassthroughSubject<Color, Never>()
-  
+
   @Published var lastColorReceived: Color = .secondary
   @Published var lastColorSent: Color = .secondary
   @Published var lastColorReply: Color = .secondary
   @Published var wcObject = WCObject()
-  
-#if os(iOS)
-  @Published var isPaired = false
-#endif
-  
+
+  #if os(iOS)
+    @Published var isPaired = false
+  #endif
+
   @Published var isReachable = false
   @Published var isCompanionAppInstalled = false
   @Published var activationState = WCSessionActivationState.notActivated
   @Published var lastError: Error?
-  
+
   var cancellables = [AnyCancellable]()
-  
+
   public func forceActivate() {
     try! wcObject.activate()
   }
-  
+
   func sendColor(_ color: Color) {
-    self.lastColorSendingSubject.send(color)
+    lastColorSendingSubject.send(color)
   }
-  
-  init () {
-    self.wcObject.activationStateSubject.assignOnMain(\.activationState, to: &$activationState)
-    self.wcObject.isReachableSubject.assignOnMain(\.isReachable, to: &$isReachable)
-    
 
+  init() {
+    wcObject.activationStateSubject.assignOnMain(\.activationState, to: &$activationState)
+    wcObject.isReachableSubject.assignOnMain(\.isReachable, to: &$isReachable)
 
-    self.wcObject.isCompanionInstalledSubject.assignOnMain(Self.isCompanionAppInstalledKeyPath, to: &$isCompanionAppInstalled)
-    
+    wcObject.isCompanionInstalledSubject.assignOnMain(Self.isCompanionAppInstalledKeyPath, to: &$isCompanionAppInstalled)
+
     #if os(iOS)
-    self.wcObject.isPairedSubject.assignOnMain(\.isPaired, to: &$isPaired)
+      wcObject.isPairedSubject.assignOnMain(\.isPaired, to: &$isPaired)
     #endif
-    
-    self.lastColorSendingSubject.share().compactMap(WCMessage.message(fromColor:)).subscribe(self.wcObject.sendingMessageSubject).store(in: &self.cancellables)
-    
-    self.wcObject.messageReceivedSubject.compactMap(self.receivedMessage(_:)).assignOnMain(to: &self.$lastColorReceived)
-    
-    let replyReceived = self.wcObject.replyMessageSubject.tryCompactMap(self.receivedReply(_:))
-    
+
+    lastColorSendingSubject.share().compactMap(WCMessage.message(fromColor:)).subscribe(wcObject.sendingMessageSubject).store(in: &cancellables)
+
+    wcObject.messageReceivedSubject.compactMap(receivedMessage(_:)).assignOnMain(to: &$lastColorReceived)
+
+    let replyReceived = wcObject.replyMessageSubject.tryCompactMap(receivedReply(_:))
+
     let (replyReceivedValue, replyReceivedError) = replyReceived.neverPublishers()
-    
-    replyReceivedError.map{$0 as Error?}.assignOnMain(to: &self.$lastError)
-        
-    replyReceivedValue.share().map{$0.0}.assignOnMain(to: &self.$lastColorSent)
-    replyReceivedValue.share().compactMap{$0.1}.assignOnMain(to: &self.$lastColorReply)
-    
-    
+
+    replyReceivedError.map { $0 as Error? }.assignOnMain(to: &$lastError)
+
+    replyReceivedValue.share().map { $0.0 }.assignOnMain(to: &$lastColorSent)
+    replyReceivedValue.share().compactMap { $0.1 }.assignOnMain(to: &$lastColorReply)
   }
-  
+
   func receivedReply(_ messageResult: WCMessageResult) throws -> (Color, Color?)? {
     let (sent, context) = messageResult
-    let reply : WCMessage?
+    let reply: WCMessage?
     switch context {
-      
     case .applicationContext:
       reply = nil
-    case .reply(let messageReply):
+
+    case let .reply(messageReply):
       reply = messageReply
-    case .failure(let error):
+
+    case let .failure(error):
       throw error
+
     case .noCompanion:
       throw MissingCompanionError()
     }
     guard let sentColor = sent.color else {
       return nil
     }
-    
+
     let replyColor = reply?.color
     return (sentColor, replyColor)
   }
-  
-  func receivedMessage(_ messageAcceptance: WCMessageAcceptance) -> Color?{
+
+  func receivedMessage(_ messageAcceptance: WCMessageAcceptance) -> Color? {
     let (message, context) = messageAcceptance
     let replyHandler = context.replyHandler
     guard let color = message.color else {
       return nil
     }
-      replyHandler?(message)
-      //print("Recv Updated: \(String(colorValue, radix: 16, uppercase: true))")
-      //lastColorReceivedSubject.send(color)
-      return color
+    replyHandler?(message)
+    // print("Recv Updated: \(String(colorValue, radix: 16, uppercase: true))")
+    // lastColorReceivedSubject.send(color)
+    return color
   }
 }
--- Shared/WCObject.swift
+++ Shared/WCObject.swift
@@ -3,47 +3,9 @@ import Foundation
 import SwiftUI
 import WatchConnectivity
 
-typealias WCMessage = [String : Any]
-
-enum WCSendMessageResult {
-  case applicationContext
-  case reply(WCMessage)
-  case failure(Error)
-  case noCompanion
-}
-
-typealias WCMessageResult = (WCMessage, WCSendMessageResult)
-
-enum WCMessageContext {
-  case replyWith(WCMessageHandler)
-  case applicationContext
-  
-  var replyHandler : WCMessageHandler? {
-    guard case let .replyWith(handler) = self else {
-      return nil
-    }
-    return handler
-  }
-}
-
-typealias WCMessageHandler = (WCMessage) -> Void
-
-
-typealias WCMessageAcceptance = (WCMessage, WCMessageContext)
-extension WCSession {
-  var isPairedAppInstalled : Bool {
-    #if os(iOS)
-    return self.isWatchAppInstalled
-    #else
-    return self.isCompanionAppInstalled
-    #endif
-  }
-}
-
 class WCObject: NSObject, WCSessionDelegate, ObservableObject {
   struct NotSupportedError: Error {}
-  var cancellable : AnyCancellable!
-
+  var cancellable: AnyCancellable!
 
   public func activate() throws {
     guard WCSession.isSupported() else {
@@ -56,7 +18,7 @@ class WCObject: NSObject, WCSessionDelegate, ObservableObject {
 
   override init() {
     super.init()
-    cancellable = self.sendingMessageSubject.sink(receiveValue: self.sendMessage(_:))
+    cancellable = sendingMessageSubject.sink(receiveValue: sendMessage(_:))
   }
 
   var _session: WCSession?
@@ -72,8 +34,7 @@ class WCObject: NSObject, WCSessionDelegate, ObservableObject {
   #if os(iOS)
     let isPairedSubject = PassthroughSubject<WCSession, Never>()
   #endif
-  
-  
+
   let messageReceivedSubject = PassthroughSubject<WCMessageAcceptance, Never>()
   let sendingMessageSubject = PassthroughSubject<WCMessage, Never>()
   let replyMessageSubject = PassthroughSubject<WCMessageResult, Never>()
@@ -92,7 +53,6 @@ class WCObject: NSObject, WCSessionDelegate, ObservableObject {
         self.isPairedSubject.send(session)
         self.isCompanionInstalledSubject.send(session)
       }
-      
     }
 
   #elseif os(watchOS)
@@ -105,15 +65,14 @@ class WCObject: NSObject, WCSessionDelegate, ObservableObject {
   func session(_ session: WCSession, activationDidCompleteWith _: WCSessionActivationState, error _: Error?) {
     _session = session
     DispatchQueue.main.async {
-      
       self.activationStateSubject.send(session)
 
       self.isReachableSubject.send(session)
       #if os(iOS)
-      self.isCompanionInstalledSubject.send(session)
-      self.isPairedSubject.send(session)
+        self.isCompanionInstalledSubject.send(session)
+        self.isPairedSubject.send(session)
       #elseif os(watchOS)
-      self.isCompanionInstalledSubject.send(session)
+        self.isCompanionInstalledSubject.send(session)
       #endif
     }
   }
@@ -122,36 +81,31 @@ class WCObject: NSObject, WCSessionDelegate, ObservableObject {
     DispatchQueue.main.async {
       self.isReachableSubject.send(session)
     }
-    
   }
-  
-  
-  
+
   private func sendMessage(_ message: WCMessage) {
-    if self.actualSession.isReachable {
+    if actualSession.isReachable {
       actualSession.sendMessage(message) { reply in
         self.replyMessageSubject.send((message, .reply(reply)))
       } errorHandler: { error in
         self.replyMessageSubject.send((message, .failure(error)))
       }
-    } else if self.actualSession.isPairedAppInstalled {
+    } else if actualSession.isPairedAppInstalled {
       do {
         try actualSession.updateApplicationContext(message)
       } catch {
-        self.replyMessageSubject.send((message, .failure(error)))
+        replyMessageSubject.send((message, .failure(error)))
 
         return
       }
-                                      self.replyMessageSubject.send((message, .applicationContext))
-      
+      replyMessageSubject.send((message, .applicationContext))
     } else {
-          self.replyMessageSubject.send((message, .noCompanion))
+      replyMessageSubject.send((message, .noCompanion))
     }
   }
 
-
   func session(_: WCSession, didReceiveMessage message: [String: Any], replyHandler: @escaping ([String: Any]) -> Void) {
-    messageReceivedSubject.send((message, .replyWith( replyHandler)))
+    messageReceivedSubject.send((message, .replyWith(replyHandler)))
   }
 
   func session(_: WCSession, didReceiveApplicationContext applicationContext: [String: Any]) {
